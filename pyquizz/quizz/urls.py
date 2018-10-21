53f0eeda46c3f96515bbf7c933b5ea9ddef67a0e
Vincent Poulailleau <vpoulailleau@gmail.com>
2018-10-21 15:57:01 +0200

    add date in URL

--- pyquizz/quizz/urls.py
+++ pyquizz/quizz/urls.py
@@ -1,21 +1,45 @@
+import re
+from datetime import datetime
+
 from django.urls import path, register_converter
-from quizz.views import Home, AnswerAQuestion
+
+from quizz.views import AnswerAQuestion, Home
 
 
 class EmailConverter:
     regex = r'\w+@\w+\.\w+'
 
     def to_python(self, value):
-        return str(value)
+        return value
 
     def to_url(self, value):
-        return str(value)
+        return value
+
+
+class DateTimeConverter:
+    regex = r'(\d{4})-(\d{2})-(\d{2})--(\d{2})-(\d{2})'
+
+    def to_python(self, value):
+        m = re.search(self.regex, value)
+        print(m.groups())
+        year, month, day, hour, minute = (int(v) for v in m.groups())
+        return datetime(
+            year,
+            month,
+            day,
+            hour,
+            minute,
+        )
+
+    def to_url(self, value):
+        return value
 
 
 register_converter(EmailConverter, 'email')
+register_converter(DateTimeConverter, 'date')
 
 
 urlpatterns = [
-    path('<email:email>/', AnswerAQuestion.as_view()),
+    path('<email:email>/<date:date>/', AnswerAQuestion.as_view()),
     path('', Home.as_view(), name='home'),
 ]
