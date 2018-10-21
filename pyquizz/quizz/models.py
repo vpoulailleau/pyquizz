df186e2269e5f3fb7a7e127c8d5960c072b0a14a
Vincent Poulailleau <vpoulailleau@gmail.com>
2018-10-21 11:17:22 +0200

    add Person model

--- pyquizz/quizz/models.py
+++ pyquizz/quizz/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Person(models.Model):
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        unique=True,
+        verbose_name='email',
+        help_text='email de la personne',
+        max_length=255,
+    )
+
+    class Meta:
+        ordering = ['email']
+        verbose_name = 'Personne'
+        verbose_name_plural = 'Personnes'
+
+    def __str__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return reverse('quizz_person_detail', args=[str(self.email)])
+
+
+# For large amounts of text, use TextField, default widget is then textarea.
