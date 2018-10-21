f670736f9384d89d1447d878e2180b0e5c33c5c9
Vincent Poulailleau <vpoulailleau@gmail.com>
2018-10-21 15:15:59 +0200

    begin views

--- pyquizz/quizz/urls.py
+++ pyquizz/quizz/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
-from quizz import views
+from django.urls import path, register_converter
+from quizz.views import Home, AnswerAQuestion
+
+
+class EmailConverter:
+    regex = r'\w+@\w+\.\w+'
+
+    def to_python(self, value):
+        return str(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+register_converter(EmailConverter, 'email')
+
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('<email:email>/', AnswerAQuestion.as_view()),
+    path('', Home.as_view(), name='home'),
 ]
--- pyquizz/quizz/views.py
+++ pyquizz/quizz/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 
-def home(request):
-    return HttpResponse("Hello, Django!")
+class Home(View):
+    def get(self, request):
+        return HttpResponse('Ça marche !')
+
+
+class AnswerAQuestion(TemplateView):
+    template_name = 'quizz/answer_a_question.html'
