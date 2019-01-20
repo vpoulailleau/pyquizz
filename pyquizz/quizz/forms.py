9ed700f5b86ad028016d458336a60409929ff2cb
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-01-20 11:32:06 +0100

    ignore question without answers

--- pyquizz/quizz/forms.py
+++ pyquizz/quizz/forms.py
@@ -19,39 +19,53 @@ class AnswerForm(forms.Form):
     answer9 = forms.BooleanField(required=False)
 
     def clean_email(self):
-        email = self.cleaned_data['email']
+        email = self.cleaned_data["email"]
         if not Person.objects.filter(email=email):
-            raise forms.ValidationError('Cet email est inconnu.')
+            raise forms.ValidationError("Cet email est inconnu.")
         return email
 
     def clean(self):
         cleaned_data = super().clean()
-        if 'quizz_sending' not in cleaned_data:
+        if "quizz_sending" not in cleaned_data:
             raise forms.ValidationError(
-                "Ce quizz n'existe pas. Vérifiez la date.")
-        if 'email' not in cleaned_data:
-            raise forms.ValidationError(
-                "Cet email est inconnu.")
+                "Ce quizz n'existe pas. Vérifiez la date."
+            )
+        if "email" not in cleaned_data:
+            raise forms.ValidationError("Cet email est inconnu.")
         quizz_sending = QuizzSending.objects.get(
-            pk=cleaned_data['quizz_sending'])
-        if not quizz_sending.group.persons.filter(
-                email=cleaned_data['email']):
-            raise forms.ValidationError(
-                "Ce quizz n'est pas fait pour vous.")
+            pk=cleaned_data["quizz_sending"]
+        )
+        if not quizz_sending.group.persons.filter(email=cleaned_data["email"]):
+            raise forms.ValidationError("Ce quizz n'est pas fait pour vous.")
+        if not any(
+            (
+                self.cleaned_data["answer0"],
+                self.cleaned_data["answer1"],
+                self.cleaned_data["answer2"],
+                self.cleaned_data["answer3"],
+                self.cleaned_data["answer4"],
+                self.cleaned_data["answer5"],
+                self.cleaned_data["answer6"],
+                self.cleaned_data["answer7"],
+                self.cleaned_data["answer8"],
+                self.cleaned_data["answer9"],
+            )
+        ):
+            raise forms.ValidationError("Aucune réponse n'a été fournie.")
 
     def add_answer_in_database(self):
         quizz_sending = QuizzSending.objects.get(
-            pk=self.cleaned_data['quizz_sending'])
-        person = Person.objects.get(email=self.cleaned_data['email'])
-        question = Question.objects.get(
-            pk=self.cleaned_data['question'])
+            pk=self.cleaned_data["quizz_sending"]
+        )
+        person = Person.objects.get(email=self.cleaned_data["email"])
+        question = Question.objects.get(pk=self.cleaned_data["question"])
 
         answers = []
         for index in range(10):
-            answer = self.cleaned_data[f'answer{index}']
+            answer = self.cleaned_data[f"answer{index}"]
             if answer:
                 answers.append(index)
-        answers = ','.join(str(answer) for answer in answers)
+        answers = ",".join(str(answer) for answer in answers)
 
         answer = Answer(
             quizz_sending=quizz_sending,
