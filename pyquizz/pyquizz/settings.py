aa64218b7e605dad6938cabdb61f54a05c9ca874
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-01-27 16:24:04 +0100

    restore static file collection

--- pyquizz/pyquizz/settings.py
+++ pyquizz/pyquizz/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "pyquizz/static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "pyquizz/static"),)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static_collected")
 
